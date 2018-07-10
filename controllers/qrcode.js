a2262e6bc27bc9b29dcf4d56db1337e2e9cf92d1
MangoGee <283959070@qq.com>
2018-07-10 21:28:59 +0800

    添加windows系统支持

--- controllers/qrcode.js
+++ controllers/qrcode.js
@@ -5,24 +5,26 @@ const Axios = require('axios')
 const config = require('../config')
 
 router.get('/getQrcode', async ctx => {
-  // 获取端口
-  const port = shell.checkWxToolPort()
-  
-  // 项目路径
-  const projectPath = encodeURIComponent(config.projectpath)
-  const previewUrl = `http://0.0.0.0:${port}/preview?projectpath=${projectPath}&format=base64`
-
   try {
-    // 调用预览接口
-    const resp = (await Axios.get(previewUrl)).data
-    ctx.body = `<img src="data:image/png;base64,${resp}" />`
-  } catch (error) {
-    const {response} = error
-    if (!response) {
-      console.info(error)
-    } else {
-      ctx.body = response.data.error
+    // 获取端口
+    const port = shell.checkWxToolPort()
+
+    // 如果未找到端口号
+    if (port === null) {
+      throw '未找到端口号，请开发人员打开微信开发者工具'
     }
+
+    // 项目路径
+    const projectPath = encodeURIComponent(config.projectpath)
+    const previewUrl = `http://0.0.0.0:${port}/preview?projectpath=${projectPath}&format=base64`
+
+    // 调用预览接口
+    const previewResp = (await Axios.get(previewUrl)).data
+    ctx.body = `<img src="data:image/png;base64,${previewResp}" />`
+  } catch (error) { // 如果异常
+    // 获取响应体
+    const {response} = error
+    ctx.body = !response ? error : response.data.error
   }
 })
 
--- shell/index.js
+++ shell/index.js
@@ -1,12 +1,21 @@
-const shell = require('shelljs')
+const OS = require('os')
+const SHELL = require('shelljs')
 
-const TOOL_PATH = '~/Library/Application\ Support/微信web开发者工具/Default/.ide'
+const TOOL_PATH = {
+  'Darwin': '~/Library/Application\ Support/微信web开发者工具/Default/.ide',
+  'Windows_NT': '~/AppData/Local/微信web开发者工具/User\ Data/Default/.ide'
+}
 
 // 获取微信开发者工具的端口
 exports.checkWxToolPort = function () {
+  // 微信开发者工具端口
   let port = null
   try {
-    port = shell.tail({'-n': 1}, TOOL_PATH).toString()
+    // 获取当前系统
+    const currOS = OS.type()
+
+    // 获取端口
+    port = SHELL.tail({'-n': 1}, TOOL_PATH[currOS]).toString()
   } catch (error) {
     console.info(error)
   } finally {
